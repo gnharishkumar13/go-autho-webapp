f1ff874eb5c7cb4472f88258909a621304361059
gnharishkumar13 <harishkumar_gn@yahoo.com>
2020-05-29 09:45:44 -0400

    health check added

--- main.go
+++ main.go
@@ -11,5 +11,11 @@ func main() {
 
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	r.Handle("/health", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("API is up and running"))
+		})).Methods("GET")
+
 	http.ListenAndServe(":3000", r)
 }
